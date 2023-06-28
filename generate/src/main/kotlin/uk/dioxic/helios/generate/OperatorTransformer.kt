f3cbc773fde1ec00119aafa6b9c8b94ee9de61ac
Mark Baker-Munton <markbm@mongodb.com>
2023-06-28 18:11:45 +0100

    REFACTOR: use when clause

--- generate/src/main/kotlin/uk/dioxic/helios/generate/OperatorTransformer.kt
+++ generate/src/main/kotlin/uk/dioxic/helios/generate/OperatorTransformer.kt
@@ -4,20 +4,22 @@ import org.bson.Document
 import org.bson.Transformer
 
 class OperatorTransformer: Transformer {
-    override fun transform(objectToTransform: Any?): Any {
-        if (objectToTransform is Document) {
-            if (objectToTransform.size == 1) {
-                val (key, value) = objectToTransform.entries.iterator().next()
-                if (OperatorFactory.canHandle(key)) {
-                    return OperatorFactory.create(key, value)
+    override fun transform(objectToTransform: Any?): Any? {
+        when (objectToTransform) {
+            is Document -> {
+                if (objectToTransform.size == 1) {
+                    val (key, value) = objectToTransform.entries.first()
+                    if (OperatorFactory.canHandle(key)) {
+                        return OperatorFactory.create(key, value)
+                    }
                 }
             }
-        } else if (objectToTransform is String) {
-            if (OperatorFactory.canHandle(objectToTransform)) {
-                return OperatorFactory.create(objectToTransform)
+            is String -> {
+                if (OperatorFactory.canHandle(objectToTransform)) {
+                    return OperatorFactory.create(objectToTransform)
+                }
             }
         }
-
-        return objectToTransform!!
+        return objectToTransform
     }
 }
