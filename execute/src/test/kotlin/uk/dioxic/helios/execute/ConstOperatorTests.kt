b8eb82e39039365f21a02c3c733196af920414f8
Mark Baker-Munton <markbm@mongodb.com>
2023-06-30 18:53:47 +0100

    TEST: test constants aren't being hydrate when they aren't needed

--- execute/src/test/kotlin/uk/dioxic/helios/execute/ConstOperatorTests.kt
+++ execute/src/test/kotlin/uk/dioxic/helios/execute/ConstOperatorTests.kt
@@ -4,58 +4,92 @@ import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.should
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
+import io.mockk.every
+import io.mockk.mockkStatic
+import io.mockk.verify
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.serialization.json.put
 import uk.dioxic.helios.execute.model.Benchmark
 import uk.dioxic.helios.execute.model.MessageExecutor
 import uk.dioxic.helios.execute.operators.ConstOperator
 import uk.dioxic.helios.execute.results.TimedMessageResult
-import uk.dioxic.helios.generate.OperatorFactory
-import uk.dioxic.helios.generate.buildTemplate
-import uk.dioxic.helios.generate.putKeyedOperator
+import uk.dioxic.helios.generate.*
+import uk.dioxic.helios.generate.operators.NameOperator
 
 class ConstOperatorTests : FunSpec({
 
     OperatorFactory.addOperator(ConstOperator::class)
 
-    val template = buildTemplate {
-        putKeyedOperator<ConstOperator>("name", "animal")
-    }
-    val consts = buildTemplate {
-        put("animal", "halibut")
-    }
-    val executor = MessageExecutor(template)
 
-    suspend fun Benchmark.verify() {
-        execute().filterIsInstance<ProgressMessage>().collect { msg ->
-            msg.result.shouldBeInstanceOf<TimedMessageResult>().value.doc.should { doc ->
-                doc["name"] shouldBe "halibut"
+    context("lookups") {
+
+        val template = buildTemplate {
+            putKeyedOperator<ConstOperator>("name", "animal")
+        }
+        val executor = MessageExecutor(template)
+        val consts = buildTemplate {
+            put("animal", "halibut")
+        }
+
+        suspend fun Benchmark.verify() {
+            execute().filterIsInstance<ProgressMessage>().collect { msg ->
+                msg.result.shouldBeInstanceOf<TimedMessageResult>().value.doc.should { doc ->
+                    doc["name"] shouldBe "halibut"
+                }
             }
+        }
+
+        test("benchmark constant lookup is successful") {
+            buildBenchmark(constants = consts) {
+                sequentialStage {
+                    rateWorkload(executor = executor)
+                }
+            }.verify()
+        }
+
+        test("stage constant lookup is successful") {
+            buildBenchmark {
+                sequentialStage(constants = consts) {
+                    rateWorkload(executor = executor)
+                }
+            }.verify()
+        }
+
+        test("workload constant lookup is successful") {
+            buildBenchmark {
+                sequentialStage {
+                    rateWorkload(constants = consts, executor = executor)
+                }
+            }.verify()
         }
     }
 
-    test("benchmark constant lookup is successful") {
+    test("only call lazy constants if necessary") {
+        mockkStatic("uk.dioxic.helios.generate.HydrationKt")
+
+        val template = buildTemplate {
+            putOperator<NameOperator>("name")
+        }
+        val executor = MessageExecutor(template)
+        val consts = buildTemplate {
+            put("animal", "halibut")
+        }
+
+        every {
+            with(any<OperatorContext>()) { consts.hydrate() }
+        } returns emptyMap<String, Any?>()
+
+
         buildBenchmark(constants = consts) {
             sequentialStage {
                 rateWorkload(executor = executor)
             }
-        }.verify()
-    }
+        }.execute().collect()
 
-    test("stage constant lookup is successful") {
-        buildBenchmark {
-            sequentialStage(constants = consts) {
-                rateWorkload(executor = executor)
-            }
-        }.verify()
-    }
-
-    test("workload constant lookup is successful") {
-        buildBenchmark {
-            sequentialStage {
-                rateWorkload(constants = consts, executor = executor)
-            }
-        }.verify()
+        verify(inverse = true) {
+            with(any<OperatorContext>()) { consts.hydrate() }
+        }
     }
 
 })
