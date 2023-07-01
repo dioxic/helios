3ee559d96963402026b4da7083c0d6c722eeab17
Mark Baker-Munton <markbm@mongodb.com>
2023-07-01 20:10:17 +0100

    FEAT(dot map): add intermediate branches to flatten map (dot map)

--- generate/src/main/kotlin/uk/dioxic/helios/generate/extensions/Map.kt
+++ generate/src/main/kotlin/uk/dioxic/helios/generate/extensions/Map.kt
@@ -5,37 +5,48 @@ import kotlinx.serialization.json.JsonObject
 import org.bson.BsonDocument
 import org.bson.BsonValue
 
-fun Map<String, Any?>.flatten(separator: Char = '.') =
+fun Map<String, Any?>.flatten(separator: Char = '.', incBranches: Boolean = true) =
     mutableMapOf<String, Any?>().also {
-        flatten(it, this, separator)
+        flatten(it, this, separator, incBranches)
     }.toMap()
 
-fun JsonObject.flatten(separator: Char = '.') =
+fun JsonObject.flatten(separator: Char = '.', incBranches: Boolean = true) =
     mutableMapOf<String, JsonElement>().also {
-        flatten(it, this, separator)
+        flatten(it, this, separator, incBranches)
     }.toMap()
 
-fun BsonDocument.flatten(separator: Char = '.') =
+fun BsonDocument.flatten(separator: Char = '.', incBranches: Boolean = true) =
     mutableMapOf<String, BsonValue>().also {
-        flatten(it, this, separator)
+        flatten(it, this, separator, incBranches)
     }.toMap()
 
 
-// TODO put complex objects into the map (maps + lists)
 @Suppress("UNCHECKED_CAST")
-private fun <T> flatten(map: MutableMap<String, T>, value: T, separator: Char, key: String = "") {
-    return when (value) {
+private fun <T> flatten(
+    map: MutableMap<String, T>,
+    value: T,
+    separator: Char,
+    incBranches: Boolean = true,
+    key: String = ""
+) {
+    when (value) {
         is Map<*, *> -> {
             value.filterValues { it != null }.forEach { (k, v) ->
                 val newKey = getKey(key, separator, k.toString())
-                flatten(map, v as T, separator, newKey)
+                flatten(map, v as T, separator, incBranches, newKey)
+            }
+            if (incBranches) {
+                map[key] = value
             }
         }
 
         is Iterable<*> -> {
             value.filterNotNull().forEachIndexed { i, v ->
                 val newKey = getKey(key, separator, i.toString())
-                flatten(map, v as T, separator, newKey)
+                flatten(map, v as T, separator, incBranches, newKey)
+            }
+            if (incBranches) {
+                map[key] = value
             }
         }
 
