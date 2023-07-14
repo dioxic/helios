511ec5697260d569a2fea1c8b252059c58c14be2
Mark Baker-Munton <dioxic@gmail.com>
2023-07-14 09:55:55 +0100

    move repository into project settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,21 @@ pluginManagement {
     }
 }
 
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+        maven {
+            url = uri("https://maven.pkg.github.com/dioxic/kotlinx-serialization-bson")
+            credentials {
+                val ghUsername: String? by settings
+                val ghToken: String? by settings
+                username = ghUsername
+                password = ghToken
+            }
+        }
+    }
+}
+
 include("generate")
 include("cli")
 include("execute")
