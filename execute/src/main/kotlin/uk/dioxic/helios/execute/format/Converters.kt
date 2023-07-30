8731717590632cf06bfd71bafd205fe93b248e99
Mark Baker-Munton <dioxic@gmail.com>
2023-07-30 09:40:32 +0100

    basic error output for reporting

--- execute/src/main/kotlin/uk/dioxic/helios/execute/format/Converters.kt
+++ execute/src/main/kotlin/uk/dioxic/helios/execute/format/Converters.kt
@@ -3,6 +3,15 @@ package uk.dioxic.helios.execute.format
 import com.mongodb.MongoBulkWriteException
 import com.mongodb.MongoException
 import com.mongodb.MongoServerException
+import com.mongodb.WriteError
+import kotlinx.serialization.bson.Bson
+import kotlinx.serialization.bson.internal.decodeFromStream
+import kotlinx.serialization.builtins.MapSerializer
+import kotlinx.serialization.builtins.serializer
+import okio.FileSystem
+import okio.Path.Companion.toPath
+import okio.buffer
+import okio.use
 import uk.dioxic.helios.execute.model.ExecutionContext
 import uk.dioxic.helios.execute.results.ResultAccumulator
 import uk.dioxic.helios.execute.results.SummarizedResult
@@ -46,6 +55,7 @@ fun SummarizedResult.toOutputResult(stageName: String) = OutputResult(
     successCount = successCount,
     failureCount = failureCount,
     operationCount = operationCount,
+    errorCount = errorCount,
     progress = context.executionProgress,
     errorDetail = exceptions.distinctBy { it::class to it.code }.joinToString(", ") { it.toOutputString() },
     elapsed = elapsedTime,
@@ -56,20 +66,30 @@ private val ExecutionContext.executionProgress
     get() = count.percentOf(workload.count)
 
 private infix fun Int.percentOf(divisor: Int): Percent = (this * 100) / divisor
-private infix fun Long.percentOf(divisor: Long): Percent {
-    println("percent: $this / $divisor")
-    return ((this * 100) / divisor).toInt()
-}
+private infix fun Long.percentOf(divisor: Long): Percent = ((this * 100) / divisor).toInt()
 
 fun MongoException.toOutputString() =
     when (this) {
         is MongoBulkWriteException -> this.toOutputString()
-        is MongoServerException -> errorCodeName.orEmpty()
+        is MongoServerException -> errorCodeName ?: getErrorName()
         else -> "${this::class.simpleName!!}[${this.message.orEmpty()}]"
     }
 
-fun MongoBulkWriteException.toOutputString(): String {
-    this.writeErrors.groupBy { it.code }
+fun MongoBulkWriteException.toOutputString(): String = buildString {
+    append(writeErrors.distinctBy { it.code }.joinToString(", ") { it.getErrorName() })
 
-    TODO()
+    writeConcernError?.also {
+        append(getCause(code))
+    }
 }
+
+fun MongoException.getErrorName() = getCause(code)
+fun WriteError.getErrorName() = getCause(code)
+
+private fun getCause(code: Int) =
+    mongoErrorCodes[code] ?: code.toString()
+
+val mongoErrorCodes =
+    FileSystem.RESOURCES.source("mongoErrorCodes.json".toPath()).buffer().use {
+        Bson.decodeFromStream(MapSerializer(String.serializer(), String.serializer()), it.inputStream())
+    }.mapKeys { (k,_) -> k.toInt() }
